2edc21374f6d3aff88759266b3f9ed73e907dde2
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2025-12-23 15:55:29 +0545

    Created and styled Navbar of hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Hero from "./components/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,16 +3,40 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="bg-[#070818] h-screen w-full">
+    <div className="bg-[#f8fafc] h-screen w-full">
       {/* NAVIGATION START */}
-      <nav className="sticky top-0 z-50  bg-gray-400 ">
-        <div className="max-w-7xl  px-3">
+      <nav className="sticky top-0 z-50 ">
+        <div className="max-w-8xl  px-6">
           <div className="flex items-center h-16 justify-between">
-            {/* LOGO DIV */}
-            <Link to="/" className="flex gap-2 text-xl font-bold">
-              <img src={Logo} alt="Pravidhi logo" className="h-8" />
+            {/*  LEFT DIV LOGO  */}
+            <Link
+              to="/"
+              className="flex items-center gap-2 text-3xl text-[#0F172A] font-bold"
+            >
+              <img src={Logo} alt="Pravidhi logo" className="h-8 rounded-lg" />
               <span>Pravidhi</span>
             </Link>
+
+            {/* MIDDLE LINKS  */}
+            <div className="hidden md:flex items-center gap-8 text-[#0F172A] text-sm md:text-xl font-medium">
+              <Link to="/" className="hover:text-[#1D70B8] transition">
+                Home
+              </Link>
+              <Link to="/" className="hover:text-[#1D70B8] transition">
+                About Us
+              </Link>
+              <Link to="/" className="hover:text-[#1D70B8] transition">
+                Blog
+              </Link>
+              <Link to="/" className="hover:text-[#1D70B8] transition">
+                Contact
+              </Link>
+            </div>
+
+            {/* RIGHT DIV CTA BUTTON  */}
+            <div className="flex items-center">
+              <button className="rounded-xl px-6">Get Started</button>
+            </div>
           </div>
         </div>
       </nav>
