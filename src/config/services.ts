3843d371602b9861ec61ffbb66e45b305c865645
Gamma <221785314+Coder-Gamma@users.noreply.github.com>
2026-01-04 13:10:36 +0545

    Created a data array for services

---
+++ src/config/services.ts
@@ -0,0 +1,33 @@
+import { Globe, Smartphone, Code2, Briefcase, Truck } from "lucide-react";
+
+export const SERVICES_DATA = [
+  {
+    title: "Website Development",
+    description:
+      "We build fully responsive and aesthetic websites that not just looks beautiful but functions beautifully as well.",
+    icon: Globe,
+  },
+  {
+    title: "App Development",
+    description: "We will build the app of your dreams, Just contact us.",
+    icon: Smartphone,
+  },
+  {
+    title: "Software Development",
+    description:
+      "Pravidhi-the top IT company in Pokhara will deliver reliable and custom softwares as per the business needs.",
+    icon: Code2,
+  },
+  {
+    title: "IT Consulting",
+    description:
+      "Our manager Mr.Bishal sir is the best consultant you will find all over the valley. Don't miss out.",
+    icon: Briefcase,
+  },
+  {
+    title: "International Courier",
+    description:
+      "Need to send your long distance girlfriend a letter ? a gift ? No worries! We got you covered.",
+    icon: Truck,
+  },
+];
